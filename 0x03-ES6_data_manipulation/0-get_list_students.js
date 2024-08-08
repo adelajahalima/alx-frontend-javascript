3988a75a40509f6a207f9f087df9195ccb947af7
adelajahalima <enioladelajah@gmail.com>
2024-08-08 14:46:18 +0100

    Added file for task 0

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,4 +1,4 @@
-import default function getListStudents() {
+export default function getListStudents() {
   return [
     { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
     { id: 2, firstName: 'James', location: 'Columbia' },
