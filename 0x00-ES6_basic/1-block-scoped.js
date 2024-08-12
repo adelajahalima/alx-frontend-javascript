584960bc82567d8a162963c298be9068284897f0
adelajahalima <adelajahalima@gmail.com>
2024-08-12 15:34:15 +0100

    Added file for task 2

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -3,9 +3,7 @@ export default function taskBlock(trueOrFalse) {
   const task2 = true;
 
   if (trueOrFalse) {
-    const innerTask = true; // This task is scoped to the if block
-    const innerTask2 = false; // This task2 is scoped to the if block
-    return [innerTask, innerTask2];
+    return [true, false];
   }
 
   return [task, task2]; // The return statement accesses the outer task and task2 variables
